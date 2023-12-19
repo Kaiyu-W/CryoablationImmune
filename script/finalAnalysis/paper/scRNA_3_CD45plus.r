210b0fe0d311536a6147af43988b7d84fe3bd118
Kaiyu-W <79740043+Kaiyu-W@users.noreply.github.com>
2023-12-19 14:29:31 +0800

    Add files via upload

--- script/finalAnalysis/paper/scRNA_3_CD45plus.r
+++ script/finalAnalysis/paper/scRNA_3_CD45plus.r
@@ -218,3 +218,239 @@ ggsave(
     plot = CD45plus_count_Plot,
     height = 6, width = 3.4
 )
+
+# 1.4 to find where I-IFN signals come from
+makeTable <- function(GO_result, width = 60) {
+    merge <- function(vec, len, sep = "\n") {
+        if (length(vec) < len) {
+            paste(vec, collapse = "")
+        } else {
+            x1 <- paste(vec[1:len], collapse = "")
+            x2 <- merge(vec[(len + 1):length(vec)], len)
+            paste(x1, x2, sep = sep)
+        }
+    }
+
+    Description <- sapply(GO_result$Description, function(x) {
+        y <- strsplit(x, split = "")[[1]]
+        y[1] <- toupper(y[1])
+        paste(y, collapse = "")
+    })
+    if (sum(nchar(Description) > width) == 0) {
+        Description <- sapply(Description, function(x) {
+            ifelse(
+                nchar(x) < width,
+                paste0(paste(rep(" ", width - nchar(x)), collapse = ""), x),
+                x
+            )
+        })
+    }
+    Description <- sapply(Description, function(x) {
+        if (nchar(x) <= width) {
+            x
+        } else {
+            y <- strsplit(x, "")[[1]]
+            merge(y, width, "\n")
+        }
+    })
+
+    Description <- factor(
+        Description,
+        levels = rev(Description)
+    )
+    pvalue <- GO_result$p.adjust
+    df <- data.frame(
+        name = Description,
+        pvalue = -log10(pvalue)
+    )
+    return(df)
+}
+########### 
+# by GSVA, with all cell types
+library(GSVA)
+library(GSEABase)
+source("~/Desktop/Github/My_scRNA_pipeline/Pathway_score.r")
+red <- "#ff5656" # "red"
+blue <- "#5858ff" # "blue"
+RWB_color <- grDevices::colorRampPalette(colors = c(blue, "white", red))(100)
+GSV_path <- "/mnt/c/DeepTL_dev-main/pathway/GSVA/"
+gmt_file <- paste0(GSV_path, "c5.go.v7.4.symbols1.gmt")
+geneset <- getGmt(gmt_file)
+pathway_names <- names(geneset)[grepl("interferon.*production", names(geneset), ignore.case = TRUE)]
+geneset_IIFN <- geneset[pathway_names]
+
+Cryo_merge_tmp1 <- Cryo_merge
+Cryo_merge_tmp1 <- geneset_score(
+    Cryo_merge_tmp1, geneset_IIFN, 
+    slot = "data", highly_variable = T,
+    method = 'GSVA', 
+    gsva_method = 'ssgsea', # "gsva", "ssgsea", "zscore", "plage"
+    gsva_kcdf = 'Gaussian'
+)
+pathway_names <- pathway_names[!grepl("gamma", pathway_names, ignore.case = TRUE)]
+pathway_names <- pathway_names[!grepl("regulation", pathway_names, ignore.case = TRUE)]
+p_dot <- my_DotPlot_split(
+    Cryo_merge_tmp1, 
+    features = pathway_names, 
+    group.by = 'MainCluster', 
+    cols = 'RdBu',
+    scale.min = 0
+) + coord_flip() + RotatedAxis() + theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(), 
+    # legend.position = 'bottom'
+) + guides(size = FALSE)
+ggsave(
+    filename = "plot/scRNA3_CD45plus_I-IFN_prod_dot.pdf",
+    plot = p_dot,
+    height = 2.5, width = 10
+)
+
+# CA_vs_Non-CA within each cell type
+Idents(Cryo_merge) <- "MainCluster"
+obj_list <- list()
+for (g in unique(Cryo_merge$MainCluster)) {
+    sub_obj <- subset(Cryo_merge, ident = g)
+    Idents(sub_obj) <- "orig.ident2"
+    Markers_sub_list <- FindAllMarkers(sub_obj, only.pos = T)
+    Markers_sub_df <- my_Markers2df_multiple(
+        Markers_sub_list,
+        logFC_threshold = 0.25,
+        positive = T,
+        n_top = 120
+    )
+    GO_ <- my_GO(
+        Markers_sub_df$Cluster_Cryo,
+        return_plot = F, return_res = T,
+        ont = "BP", Simplify = F, type = "bar", font.size = 18, show = 30, 
+        pvalue = 1, qvalue = 1
+    )
+    obj_list[[g]] <- GO_@result[grepl('interferon', GO_@result$Description),]
+}
+
+ifn_prod_pathways <- sort(unique(unlist(lapply(
+    obj_list,
+    function(x) {
+        y <- x$Description
+        y[grepl('production',y)&!grepl('gamma',y)&!grepl('regulation',y)]
+    }
+))))
+ifn_prod_matrix_p <- matrix(
+    1, 
+    nrow = length(ifn_prod_pathways), 
+    ncol = length(obj_list), 
+    dimnames = list(
+        ifn_prod_pathways, 
+        names(obj_list)
+    )
+)
+ifn_prod_matrix_q <- matrix(
+    1, 
+    nrow = length(ifn_prod_pathways), 
+    ncol = length(obj_list), 
+    dimnames = list(
+        ifn_prod_pathways, 
+        names(obj_list)
+    )
+)
+for (i in names(obj_list)) {
+    res_ <- obj_list[[i]]
+    for (j in ifn_prod_pathways) {
+        if (j %in% res_$Description) {
+            padj <- res_[res_$Description == j, 'p.adjust']
+            q_v <- res_[res_$Description == j, 'qvalue']
+            ifn_prod_matrix_p[j,i] <- -log10(padj)
+            ifn_prod_matrix_q[j,i] <- q_v
+        }
+    }
+}
+
+p_mat <- ComplexHeatmap::Heatmap(
+    mat = ifn_prod_matrix_p,
+    name = '-Log10(p.adj)\n*>1.3',
+    show_heatmap_legend = TRUE,
+    cell_fun = function(j, i, x, y, w, h, col) {
+        grid.text(
+            ifelse(
+                ifn_prod_matrix_q <= 0.2,
+                ifelse(
+                    ifn_prod_matrix_p >= -log10(0.001),
+                    '***',
+                    ifelse(
+                        ifn_prod_matrix_p >= -log10(0.01),
+                        '**',
+                        ifelse(
+                            ifn_prod_matrix_p >= -log10(0.05),
+                            '*',
+                            ''
+                        )
+                    )
+                ),
+                ''
+            )[i, j], 
+            x, y
+        )
+    },
+    col = colorRampPalette(rev(brewer.pal(n = 7, name ="PRGn")))(100),
+    rect_gp = gpar(col = "white", lwd = 1),
+    gap = unit(0.05, "inches"),
+    column_gap = unit(0.05, "inches"),
+    row_names_gp = gpar(fontsize = 12),
+    width = unit(6, "cm"),
+    height = unit(4, "cm"), 
+    heatmap_legend_param = list(direction = "horizontal"),
+    column_names_rot = 0,
+    column_names_centered = TRUE
+)
+p_mat
+
+# save
+library(showtext)
+font_add("Arial", "/usr/share/fonts/windows10/arial.ttf")
+font_add("Bold", "/usr/share/fonts/windows10/arialbd.ttf")
+showtext_auto()
+setFont <- function(p, font_family = "Arial", legend_font_family = "Bold") {
+    p@column_names_param$gp$fontfamily <- font_family # 列名称
+    p@column_names_param$gp$fontsize <- 12
+
+    p@row_names_param$gp$fontfamily <- font_family # 行名称
+    p@row_names_param$gp$fontsize <- 12
+
+    p
+}
+pdf("plot/scRNA3_CD45plus_I-IFN_heatmap.pdf", width = 6, height = 5)
+draw(
+    setFont(p_mat),
+    heatmap_legend_side = 'top'
+)
+dev.off()
+
+
+# IIFN_prod_genes_ref <- unique(unlist(lapply(geneset[pathway_names], function(x) x@geneIds)))
+# allgenes <- rownames(Cryo_merge)
+# # allgenes <- Myeloid_sub_real@assays$SCT@var.features
+# IIFN_prod_genes <- allgenes[which(toupper(allgenes) %in% IIFN_prod_genes_ref)]
+
+# p_dotx <- my_DotPlot_split(
+#     Cryo_merge_tmp1, 
+#     features = IIFN_prod_genes, 
+#     group.by = 'MainCluster',
+#     # cols = 'RdBu',
+#     scale.min = 0
+# ) + theme(
+#     axis.title.x = element_blank(),
+#     axis.title.y = element_blank(), 
+#     # legend.position = 'bottom'
+# ) + guides(size = FALSE)
+# p1<-my_Heatmap(
+#     Cryo_merge[,Cryo_merge$orig.ident2 == 'Cryo'],
+#     gene=IIFN_prod_genes,group.by = 'MainCluster',slot='scale.data',
+#     show_rownames = TRUE, cluster_rows = TRUE
+# )
+# p2<-my_Heatmap(
+#     Cryo_merge[,Cryo_merge$orig.ident2 == 'NonCryo'],
+#     gene=IIFN_prod_genes,group.by = 'MainCluster',slot='scale.data',
+#     show_rownames = TRUE, cluster_rows = TRUE
+# )
+# p1+p2
+###########
